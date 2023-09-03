58936598a05655814d011c8ba0d72de43204afd1
neerubhandari <nirubhandari45@gmail.com>
2023-09-03 17:11:54 +0545

    feat : godoenv added

--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/neerubhandari/restaurant-management/database"
+	"github.com/neerubhandari/restaurant-management/models"
 
 	// "github.com/neerubhandari/restaurant-management/middleware"
 	"github.com/neerubhandari/restaurant-management/routes"
 )
 
 func main() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	database.Connect()
+	if err := database.DB.AutoMigrate(&models.Menu{}); err != nil {
+		log.Fatalf("Error during migration: %v", err)
+	}
+
+	fmt.Println("Migration successful!")
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
 	}
-	router := gin.New()
+	router := gin.Default()
 	router.Use(gin.Logger())
 	// routes.UserRoutes(router)
 	// router.Use(middleware.Authentication())
